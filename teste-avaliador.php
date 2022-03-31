40b923027dc96af840a3053dbd577ef6d5e02573
pedrosobucki <pedrosobucki@gmail.com>
2022-03-31 11:48:22 -0300

    testes automatizados

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -19,4 +19,8 @@ $leiloeiro = new Avaliador();
 $leiloeiro->avalia($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
-echo $maiorValor;
+
+$valorEsperado = 2500;
+
+$result = ($valorEsperado == $maiorValor) ? "TESTE OK!" : "TESTE FALHOU!";
+echo $result;
